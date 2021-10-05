57b34c2adbc5e2194ee50ca3d440cb262866f7d2
daoraCode <severin.mboukou@gmail.com>
2021-10-05 16:22:44 +0200

    if else  applied for Shifumi Game

--- index.js
+++ index.js
@@ -4,16 +4,16 @@ var containerIA = document.getElementById('containerIA');
 document.body.appendChild(containerIA);
 var imgIA = document.getElementById('imgIA');
 imgIA.setAttribute('src', 'stone.png');
+
 var containerPlayer = document.getElementById('containerPlayer');
 document.body.appendChild(containerPlayer);
 var imgPlayer = document.getElementById('img');
 imgPlayer.setAttribute('src', 'stone.png');
+// Game start with stone by default
 
 function choiceIA() {
   var array = ['stone', 'paper', 'scissors'];
   var random = Math.floor(Math.random() * 3);
-  console.log(random);
-  console.log(array[random]);
   if (array[random] === 'stone') {
     imgIA.setAttribute('src', 'stone.png');
   } else if (array[random] === 'paper') {
@@ -21,13 +21,12 @@ function choiceIA() {
   } else if (array[random] === 'scissors') {
     imgIA.setAttribute('src', 'scissors.png');
   }
-  console.log(array[random]);
-  return array[random];
+  var arrayChoiceIA = array[random];
+  return arrayChoiceIA;
 }
 
-function player(choice) {
-  console.log('message parametre choice: ', choice); 
-  switch(choice) {
+function player(myChoice) {
+  switch (myChoice) {
     case 'stone':
       imgPlayer.setAttribute('src', './stone.png');
       break;
@@ -40,45 +39,34 @@ function player(choice) {
     default:
       console.log('DRAW');
   }
+
   var lastChoiceIA = choiceIA();
-  console.log("lastChoiceIA variable : ", lastChoiceIA);
+
+  if (lastChoiceIA === 'stone' && myChoice === 'scissors') {
+    console.log("VOUS AVEZ PERDU, L'IA À GAGNÉ!");
+  } else if(myChoice === 'stone' && lastChoiceIA === 'scissors') {
+    console.log('VOUS AVEZ GAGNÉ!');
+  } else if(lastChoiceIA === 'scissors' && myChoice === 'paper') {
+    console.log("VOUS AVEZ PERDU, L'IA À GAGNÉ!");
+  } else if(myChoice === 'scissors' && lastChoiceIA === 'paper') {
+    console.log('VOUS AVEZ GAGNÉ!');   
+  } else if(lastChoiceIA === 'paper' && myChoice === 'stone') {
+    console.log("VOUS AVEZ PERDU, L'IA À GAGNÉ!"); 
+  } else if(myChoice === 'paper' && lastChoiceIA === 'stone') {
+    console.log('VOUS AVEZ GAGNÉ!'); 
+  } else if(myChoice === 'stone' && lastChoiceIA === 'stone') {
+    console.log('DRAW');
+  } else if(myChoice === 'scissors' && lastChoiceIA === 'scissors') {
+    console.log('DRAW');
+  } else if(myChoice === 'paper' && lastChoiceIA === 'paper') {
+    console.log('DRAW');
+  }
+
+  // if(myChoice === 5) {
+    
+  // }
+
+  // if(lastChoiceIA === 5) {
+
+  // }
 }
-
-
-function resultScore() {
-
-}
-
-// console.log(player('stone'));
-
-// function scissors() {
-//   img.setAttribute('src', 'scissors.png');
-// }
-
-// function paper() {
-//     var choices = [img, imgIA];
-//     var choice1;
-//     choice1 = img.setAttribute('src', 'paper.png');
-//     choice2 = imgIA.setAttribute('src', 'stone.png');
-// }
-
-// function scissors() {
-//     var choices = [img, imgIA];
-//     var choice1;
-//     choice1 = img.setAttribute('src', 'scissors.png');
-//     choice2 = imgIA.setAttribute('src', 'stone.png');
-// }
-
-// je choisi la pierre
-
-// l'IA choisit entre la pierre, la feuille ou les ciseaux
-// if (img) {
-// ccondition disant que : si la pierre est choisie, du coup génère moi l'un des 3 symboles : Pierre, feuille, ciseaux
-// var imgIA = document.createElement('imgIA');
-// var element = document.getElementsByClassName('container-1');
-// } else {
-//   (alert("DRAW"));
-// }
-
-// element.choice1 = img.setAttribute('src', 'stone.png');
-// choice2 = imgIA.setAttribute('src', 'stone.png');
